72b03fa69e336582cd9ec44b321c07541faf9c51
Shovar <spectre9300@gmail.com>
2026-08-12 13:06:48 +0200

    Remove obsolete log files and update latest build references

--- build/collaborative_detection/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/Users/toni/robostack/.pixi/envs/jazzy':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/Users/toni/tfm_meaconing_ws/install/collaborative_detection'
